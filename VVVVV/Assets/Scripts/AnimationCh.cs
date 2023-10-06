5f0f7e776f045c24874672142b0409827f8ceef0
Escudero22 <joan.escudero.7e6@itb.cat>
2023-10-06 16:54:01 +0200

    se cae jiji

--- VVVVV/Assets/Scripts/AnimationCh.cs
+++ VVVVV/Assets/Scripts/AnimationCh.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animation : MonoBehaviour
+public class AnimationCh : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private Animator _animator;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    private AudioSource _as;
+
+    // Start is called before the first frame update
+    void Awake()
     {
-        
+
+        _animator = gameObject.GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _as = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Personaje se mueve a la derecha
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            _rb.velocity = new Vector2(7, 0);
+            _animator.SetBool("secorre", true);
+            _sr.flipX = false;
+
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            _rb.velocity = Vector2.zero;
+            _animator.SetBool("secorre", false);
+
+        }
+        //Personaje se mueve a la izquierda
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            _rb.velocity = new Vector2(-7, 0);
+            _sr.flipX = true;
+            _animator.SetBool("secorre", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            _rb.velocity = Vector2.zero;
+            _animator.SetBool("secorre", false);
+
+        }
     }
 }
