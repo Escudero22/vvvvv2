d453d21e97a4d8db463a4304021b765ccb5c8fb2
Escudero22 <joan.escudero.7e6@itb.cat>
2023-10-19 13:27:52 +0200

    espero que funcione xd

---
+++ VVVVV/Assets/Scripts/ChangeSceneOnCollision.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnCollision : MonoBehaviour
+{
+    public static int sceneNum = 1;
+    public Transform player;
+
+    /*private void OnTriggerEnter2D(Colider2D other)
+    {
+        if (other.CompareTag("Player")) {
+
+            SceneManager.LoadScene(sceneNum++);
+        }
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.name)
+        {
+            case "nuevaescena":
+                sceneNum++;
+                SceneManager.LoadScene(sceneNum);
+                player.position = new Vector3(-10, -15, 0);
+                DontDestroyOnLoad(gameObject);
+                break;
+            case "Return Level":
+                sceneNum--;
+                SceneManager.LoadScene(sceneNum);
+                player.position = new Vector3(10, -15, 0);
+                break;
+            case "NextLevelUp":
+                sceneNum++;
+                SceneManager.LoadScene(sceneNum);
+                player.position = new Vector3(8, -15, 0);
+                DontDestroyOnLoad(gameObject);
+                break;
+            case "ReturnLevelDown":
+                sceneNum--;
+                SceneManager.LoadScene(sceneNum);
+                player.position = new Vector3(8, -10, 0);
+                break;
+            case "NextLevelL":
+                sceneNum++;
+                SceneManager.LoadScene(sceneNum);
+                player.position = new Vector3(10, -15, 0);
+                DontDestroyOnLoad(gameObject);
+                break;
+            case "ReturnLevelR":
+                sceneNum--;
+                SceneManager.LoadScene(sceneNum);
+                player.position = new Vector3(-10, -15, 0);
+                break;
+        }
+    }
+}
--- VVVVV/Assets/Scripts/MenuInicia.cs
+++ VVVVV/Assets/Scripts/MenuInicia.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EscenaMuerte : MonoBehaviour
+public class MenuInicia : MonoBehaviour
 {
-   public void VolverAJugar()
+   public void JugarM()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
-    public void Salir()
+    public void SalirI()
     {
         Debug.Log("salir");
         Application.Quit();
