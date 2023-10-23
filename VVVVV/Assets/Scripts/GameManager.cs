94f609de2fbe13e9c710245dc08f04783a47957b
Escudero22 <joan.escudero.7e6@itb.cat>
2023-10-23 15:47:05 +0200

    aaa

--- VVVVV/Assets/Scripts/GameManager.cs
+++ VVVVV/Assets/Scripts/GameManager.cs
@@ -38,26 +38,7 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
 
         }
-        if (collision.gameObject.CompareTag("NextLevelUp"))
-        {
-
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            // Encuentra el objeto Empty en la escena actual y toma su posición
-            GameObject emptyObject = GameObject.Find("Empty");
-            if (emptyObject != null)
-            {
-                Vector3 emptyPosition = emptyObject.transform.position;
-                // Solo ajusta la coordenada X del jugador
-                player.position = new Vector3(player.position.x, emptyPosition.y, player.position.z);
-            }
-            else
-            {
-                Debug.LogError("No se encontró un objeto Empty con el nombre 'Empty' en la escena actual.");
-            }
-            //// Imprimir el nombre de la escena actual
-            Debug.Log("Nombre de la escena actual: " + SceneManager.GetActiveScene().name);
-            DontDestroyOnLoad(gameObject);
-        }
+       
 
         if (collision.gameObject.CompareTag("ReturnLevel"))
         {
@@ -81,25 +62,6 @@ public class GameManager : MonoBehaviour
             
             // Gira al jugador en el eje X
             // Encuentra el objeto Empty en la escena actual y toma su posición
-        }
-        if (collision.gameObject.CompareTag("ReturnLevelDown"))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-
-            GameObject emptyObject = GameObject.Find("Empty2");
-            if (emptyObject != null)
-            {
-                Vector3 emptyPosition = emptyObject.transform.position;
-                // Solo ajusta la coordenada X del jugador
-                player.position = new Vector3(player.position.x, emptyPosition.y, player.position.z);
-            }
-            else
-            {
-                Debug.LogError("No se encontró un objeto Empty con el nombre 'Empty' en la escena actual.");
-            }
-            //// Imprimir el nombre de la escena actual
-            Debug.Log("Nombre de la escena actual: " + SceneManager.GetActiveScene().name);
-            DontDestroyOnLoad(gameObject);
         }
 
     }
