54e616cf04b6ff54911faee5ea75820a3dd1eaac
Escudero22 <joan.escudero.7e6@itb.cat>
2023-10-24 16:42:42 +0200

    dsasdadwsd
    Sasasdas

--- VVVVV/Assets/Scripts/GameManager.cs
+++ VVVVV/Assets/Scripts/GameManager.cs
@@ -8,34 +8,28 @@ public class GameManager : MonoBehaviour
     public Transform player;
     public static GameManager gameManager;
 
-    //private void Awake()
-    //{
-    //    if (GameManager.gameManager != null && GameManager.gameManager != this)
-    //        Destroy(this.gameObject);
-    //    GameManager.gameManager = this;
+    private void Awake()
+    {
+        //    if (GameManager.gameManager != null && GameManager.gameManager != this)
+        //        Destroy(this.gameObject);
+        //    GameManager.gameManager = this;
+        if (GameManager.gameManager == null)
+            GameManager.gameManager = this;
+        else
+        {
+            Destroy(this.gameObject);
+        }
 
-    //}
+        DontDestroyOnLoad(gameObject);
+
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("NextLevel"))
         {
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            // Encuentra el objeto Empty en la escena actual y toma su posición
-            GameObject emptyObject = GameObject.Find("Empty");
-            if (emptyObject != null)
-            {
-                Vector3 emptyPosition = emptyObject.transform.position;
-                // Solo ajusta la coordenada X del jugador
-                player.position = new Vector3(emptyPosition.x, player.position.y, player.position.z);
-            }
-            else
-            {
-                Debug.LogError("No se encontró un objeto Empty con el nombre 'Empty' en la escena actual.");
-            }
-            //// Imprimir el nombre de la escena actual
-            Debug.Log("Nombre de la escena actual: " + SceneManager.GetActiveScene().name);
-            DontDestroyOnLoad(gameObject);
+            
 
         }
        
@@ -43,25 +37,6 @@ public class GameManager : MonoBehaviour
         if (collision.gameObject.CompareTag("ReturnLevel"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-
-
-                // Encuentra el objeto Empty en la escena actual y toma su posición
-                GameObject emptyObject = GameObject.Find("Empty2");
-                if (emptyObject != null)
-                {
-
-                    Vector3 emptyPosition = emptyObject.transform.position;
-                    // Solo ajusta la coordenada X del jugador
-                    player.position = new Vector3(emptyPosition.x, player.position.y, player.position.z);
-
-                }
-                else
-                {
-                    Debug.LogError("No se encontró un objeto Empty con el nombre 'Empty' en la escena actual.");
-                }
-            
-            // Gira al jugador en el eje X
-            // Encuentra el objeto Empty en la escena actual y toma su posición
         }
 
     }
---
+++ VVVVV/Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public Transform _transform;
+    public GameObject spawnPointEmpty; // Asigna esto en el Inspector de Unity
+    public GameObject spawnPointEmpty2; // Asigna esto en el Inspector de Unity
+    bool NextScene = true;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        _transform = GetComponent<Transform>();
+
+        spawnPointEmpty = GameObject.Find("Empty");
+        spawnPointEmpty2 = GameObject.Find("Empty2");
+
+        if (NextScene == true ) _transform.position = spawnPointEmpty.transform.position;
+        else _transform.position = spawnPointEmpty2.transform.position;
+
+        Debug.Log(NextScene);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("NextLevel")) NextScene = true;
+        else if (collision.gameObject.CompareTag("ReturnLevel")) NextScene = false;
+
+    }
+
+    //private void Start()
+    //{
+    //    GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+    //    if (player != null)
+    //    {
+    //        if (NextScene) {
+    //            if (gameObject.CompareTag("Empty"))
+    //            {
+    //                player.transform.position = spawnPointEmpty.position;
+    //            }
+
+    //        }
+    //        else if (gameObject.CompareTag("Empty2"))
+    //        {
+    //            player.transform.position = spawnPointEmpty2.position;
+    //        }
+    //        NextScene=!NextScene;
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("No se encontró un objeto con la etiqueta 'Player'. Asegúrate de que el personaje tenga la etiqueta correcta.");
+    //    }
+    //}
+}
